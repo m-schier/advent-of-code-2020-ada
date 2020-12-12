ad9ced0761f9fb1609ed0b5b9fd4a28fd1a4cafc
Maximilian Schier <23127402+m-schier@users.noreply.github.com>
2020-12-12 17:34:02 +0100

    Fixup remove unused

--- 12/rain.ads
+++ 12/rain.ads
@@ -4,7 +4,6 @@ package Rain is
     -- Our coordinate system is +X towards East, +Y towards South
     -- List orientations in order of ascending angle, thus +X turns towards +Y and direction is CW
     type Orientation is (North, East, South, West);
-    type Rotational_Direction is (Left, Right);
 
     type Vector is record
         X, Y : Integer := 0;
@@ -15,13 +14,12 @@ package Rain is
     function To_Vector(Heading: Orientation) return Vector;
     function Rotate(Heading: Orientation; CW_Angle: Integer) return Orientation;
     function Rotate(V: Vector; CW_Angle: Integer) return Vector;
+    function Manhattan(V: Vector) return Natural;
 
     type State is record
         Position : Vector;
         Heading  : Orientation := East;
     end record;
-
-    function Manhattan(V: Vector) return Natural;
 
     function Navigate_First return Natural;
     function Navigate_Second return Natural;
